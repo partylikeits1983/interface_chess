9e5acbf04bce3d6190e1c51a2c1505c88a5db0c3
socrates <aleqvids@gmail.com>
2023-05-04 00:41:47 +0300

    eod2

--- app/matches/page.tsx
+++ app/matches/page.tsx
@@ -1,7 +1,41 @@
+import CardList from './view-matches';
+
+const cards = [
+  {
+    matchAddress: '0x123',
+    opponentAddress: '0x123',
+    wagerToken: 'USDC',
+    wagerAmount: 100,
+    timePerMove: 3600,
+    numberOfGames: 3,
+    isPending: false,
+  },
+  {
+    matchAddress: '0x124',
+    opponentAddress: '0x123',
+    wagerToken: 'USDC',
+    wagerAmount: 1000,
+    timePerMove: 4800,
+    numberOfGames: 3,
+    isPending: false,
+  },
+  {
+    matchAddress: '0x125',
+    opponentAddress: '0x123',
+    wagerToken: 'USDC',
+    wagerAmount: 500,
+    timePerMove: 1200,
+    numberOfGames: 5,
+    isPending: true,
+  },
+];
+
 export default function Page() {
   return (
     <div className="prose prose-sm prose-invert max-w-none">
       <h1 className="text-xl font-bold">Matches</h1>
+
+      <CardList cards={cards} />
     </div>
   );
 }
--- app/matches/view-matches.tsx
+++ app/matches/view-matches.tsx
@@ -1,21 +1,56 @@
 'use client';
-
-import { useState, useEffect } from 'react';
-
 const { ethers } = require('ethers');
 const { getAllWagers } = require('ui/wallet-ui/api/form');
 
-export const MatchesTable = () => {
-  const [matches, setMatches] = useState<string[]>([]);
+import React from 'react';
+import { Box, Heading, Text, Stack, ChakraProvider } from '@chakra-ui/react';
 
-  const getWagers = async (move: any) => {
-    try {
-      let value = await getAllWagers();
-      setMatches([...matches, value]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+interface Card {
+  matchAddress: string;
+  opponentAddress: string;
+  wagerToken: string;
+  wagerAmount: number;
+  timePerMove: number;
+  numberOfGames: number;
+  isPending: boolean;
+}
 
-  return <div></div>;
+interface Props {
+  cards: Card[];
+}
+
+const CardList: React.FC<Props> = ({ cards }) => {
+  return (
+    <ChakraProvider>
+      <Box>
+        <Heading as="h2" size="lg" mb={4}>
+          Challenges List
+        </Heading>
+        <Stack spacing={4}>
+          {cards.map((card, index) => (
+            <Box
+              key={index}
+              bg="black"
+              border="1px solid white"
+              borderRadius="md"
+              p={4}
+              color="white"
+            >
+              <Heading as="h3" size="md" mb={2}>
+                Match: {card.matchAddress}
+              </Heading>
+              <Text>Oppenent: {card.opponentAddress}</Text>
+              <Text>Wager Token: {card.wagerToken}</Text>
+              <Text>Wager Amount: {card.wagerAmount}</Text>
+              <Text>Time Per Move: {card.timePerMove}</Text>
+              <Text>Number of Games: {card.numberOfGames}</Text>
+              <Text>Status: {card.isPending}</Text>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+    </ChakraProvider>
+  );
 };
+
+export default CardList;
