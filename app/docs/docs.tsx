e5af1b8a9d671ba1e047c34aa7a16329047b6400
socrates <aleqvids@gmail.com>
2023-05-13 17:14:05 +0300

    updates

--- app/docs/docs.tsx
+++ app/docs/docs.tsx
@@ -2,7 +2,7 @@
 
 import {
   Box,
-  OrderedList,
+  Flex,
   ListItem,
   Text,
   Link,
@@ -12,9 +12,29 @@ import {
 export default function Docs() {
   return (
     <ChakraProvider>
-      <Link href="https://docs.chess.fish" isExternal>
-        Go to docs.chess.fish
-      </Link>
+      <Flex flexDirection="column" alignItems="center">
+        <Box mb={2}>
+          <img
+            src="/chessDark__50.png"
+            alt="Placeholder Image"
+            width={500}
+            height={500}
+          />
+        </Box>
+        <Link
+          href="https://docs.chess.fish"
+          isExternal
+          color="white"
+          fontWeight="bold"
+        >
+          Go to <strong style={{ color: 'white' }}>docs.chess.fish</strong>
+        </Link>
+        <Box mt={2}>
+          <span style={{ color: 'white' }}>
+            to explore the Chess.Fish Documentation
+          </span>
+        </Box>
+      </Flex>
     </ChakraProvider>
   );
 }
--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -18,7 +18,9 @@ const items = [
 export default function Page() {
   return (
     <div className="space-y-6">
-      <h1 className="text-xl font-medium text-gray-300">Coming Soon</h1>
+      <h1 className="text-xl font-medium text-gray-300">
+        Chess.fish Documentation
+      </h1>
       <Docs />
     </div>
   );
