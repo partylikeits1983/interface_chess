2a0ecd0599c0edb301f90668a603b4de79f8a660
pythagoras <aleqvids@gmail.com>
2023-06-06 17:51:25 +0300

    optimize

--- ui/wallet-ui/api/form.ts
+++ ui/wallet-ui/api/form.ts
@@ -411,6 +411,83 @@ export const GetAllWagersForPairing = async () => {
   }
 };
 
+const squareToCoordinate = [
+  'a1',
+  'b1',
+  'c1',
+  'd1',
+  'e1',
+  'f1',
+  'g1',
+  'h1',
+  'a2',
+  'b2',
+  'c2',
+  'd2',
+  'e2',
+  'f2',
+  'g2',
+  'h2',
+  'a3',
+  'b3',
+  'c3',
+  'd3',
+  'e3',
+  'f3',
+  'g3',
+  'h3',
+  'a4',
+  'b4',
+  'c4',
+  'd4',
+  'e4',
+  'f4',
+  'g4',
+  'h4',
+  'a5',
+  'b5',
+  'c5',
+  'd5',
+  'e5',
+  'f5',
+  'g5',
+  'h5',
+  'a6',
+  'b6',
+  'c6',
+  'd6',
+  'e6',
+  'f6',
+  'g6',
+  'h6',
+  'a7',
+  'b7',
+  'c7',
+  'd7',
+  'e7',
+  'f7',
+  'g7',
+  'h7',
+  'a8',
+  'b8',
+  'c8',
+  'd8',
+  'e8',
+  'f8',
+  'g8',
+  'h8',
+];
+
+function hexToMove(hexMove: number): string {
+  const fromPos: number = hexMove >> 6;
+  const toPos: number = hexMove & 0x3f;
+
+  const fromCoord: string = squareToCoordinate[fromPos];
+  const toCoord: string = squareToCoordinate[toPos];
+
+  return fromCoord + toCoord;
+}
+
 export const GetGameMoves = async (wagerAddress: string): Promise<string[]> => {
   let { provider } = await setupProvider();
   await updateContractAddresses();
@@ -425,7 +502,15 @@ export const GetGameMoves = async (wagerAddress: string): Promise<string[]> => {
 
     const algebraeicMoves = [];
     for (let i = 0; i < hexMoves.length; i++) {
-      const algebraeicMove = await chess.hexToMove(hexMoves[i]);
+      const algebraeicMove = hexToMove(hexMoves[i]);
+      /*       
+    const algebraeicMoveSC = await chess.hexToMove(hexMoves[i]);
+    if (algebraeicMoveSC != algebraeicMove) {
+        
+        console.log("SC:", algebraeicMoveSC);
+        console.log("TS:", algebraeicMove);
+      }
+ */
       algebraeicMoves.push(algebraeicMove);
     }
 
