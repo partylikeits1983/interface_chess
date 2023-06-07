7532ab8150290614a65ded93d56f3ebeb44584a0
socrates <aleqvids@gmail.com>
2023-06-07 20:40:27 +0300

    fix board glitch

--- ui/byline.tsx
+++ ui/byline.tsx
@@ -5,7 +5,7 @@ export default function Byline({ className }: { className: string }) {
     >
       <div className="flex flex-col justify-between space-y-2 rounded-lg bg-black p-3.5 lg:px-5 lg:py-3">
         <div className="flex items-center gap-x-1.5">
-          <div className="text-sm text-gray-400">Created By</div>
+          <div className="text-sm text-gray-400">Created by</div>
           <a
             className="text-gray-400 underline decoration-dotted underline-offset-4 transition-colors hover:text-gray-200"
             href="https://github.com/partylikeits1983"
