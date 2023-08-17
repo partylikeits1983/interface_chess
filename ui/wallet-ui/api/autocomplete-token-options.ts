16ef2745ca7b1dfd5894b3f3548f5dd71233c423
riemann <aleqvids@gmail.com>
2023-08-17 17:01:20 +0300

    logos

--- ui/wallet-ui/api/autocomplete-token-options.ts
+++ ui/wallet-ui/api/autocomplete-token-options.ts
@@ -29,28 +29,23 @@ const tokenAddressesByChainID: TokenAddresses = {
 const options = [
   {
     label: 'WBTC',
-    image:
-      'https://raw.githubusercontent.com/dappradar/tokens/main/ethereum/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599/logo.png',
+    image: '/tokens/wbtc.png',
   },
   {
     label: 'WETH',
-    image:
-      'https://raw.githubusercontent.com/dappradar/tokens/main/ethereum/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2/logo.png',
+    image: '/tokens/weth.png',
   },
   {
     label: 'USDT',
-    image:
-      'https://raw.githubusercontent.com/dappradar/tokens/main/ethereum/0xdac17f958d2ee523a2206206994597c13d831ec7/logo.png',
+    image: '/tokens/usdt.png',
   },
   {
     label: 'USDC',
-    image:
-      'https://raw.githubusercontent.com/dappradar/tokens/main/ethereum/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48/logo.png',
+    image: '/tokens/usdc.png',
   },
   {
     label: 'DAI',
-    image:
-      'https://raw.githubusercontent.com/dappradar/tokens/main/ethereum/0x6b175474e89094c44da98b954eedeac495271d0f/logo.png',
+    image: '/tokens/dai.png',
   },
 ];
 
