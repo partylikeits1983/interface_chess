6ce075148bab3493b462fe6a2acb079bea1714bf
riemann <aleqvids@gmail.com>
2023-10-10 13:29:22 -0400

    updates

--- ui/next-logo.tsx
+++ ui/next-logo.tsx
@@ -1,5 +1,5 @@
 export function NextLogo() {
   return (
-    <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Stockfish_icon_%282010-2020%29.png" />
+    <img src="/ChessFishLogo.png" alt="Next.js Logo" />
   );
 }
