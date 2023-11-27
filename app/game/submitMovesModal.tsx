2164ff5c58f4c2b8097f102c0fcef026942bca72
riemann <aleqvids@gmail.com>
2023-11-27 16:26:40 -0500

    lint

--- app/game/submitMovesModal.tsx
+++ app/game/submitMovesModal.tsx
@@ -76,91 +76,96 @@ const SubmitMovesModal: React.FC<SubmitMovesModalProps> = ({
   }, [gameWager]);
 
   return (
-<Modal isOpen={isOpen} onClose={onClose} isCentered>
-  <ModalOverlay />
-  <ModalContent
-    bg="black"
-    color="white"
-    border="1px"
-    borderColor="white"
-    maxWidth="2xl" // Use maxWidth instead of size
-  >
-    <ModalHeader>{wagerStatus ? "Wager Outcome" : "Game Over"}</ModalHeader>
-    <ModalCloseButton />
-    <ModalBody>
-      {wagerStatus ? (
-        <>
-          <Text fontSize="lg" fontWeight="bold" mb={3}>
-            Wager Results:
-          </Text>
-          <VStack align="start" spacing={2}>
-            <HStack>
-              <Icon as={FaUserCircle} color="blue.500" />
-              <Text fontSize="md" fontWeight="semibold">
-                Player {wagerPlayers?.player0}: <chakra.span color="green.500">{wagerStatus.winsPlayer0} Wins</chakra.span>
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent
+        bg="black"
+        color="white"
+        border="1px"
+        borderColor="white"
+        maxWidth="2xl" // Use maxWidth instead of size
+      >
+        <ModalHeader>{wagerStatus ? 'Wager Outcome' : 'Game Over'}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {wagerStatus ? (
+            <>
+              <Text fontSize="lg" fontWeight="bold" mb={3}>
+                Wager Results:
               </Text>
-            </HStack>
-            <HStack>
-              <Icon as={FaUserCircle} color="red.500" />
-              <Text fontSize="md" fontWeight="semibold">
-                Player {wagerPlayers?.player1}: <chakra.span color="green.500">{wagerStatus.winsPlayer1} Wins</chakra.span>
-              </Text>
-            </HStack>
-          </VStack>
-        </>
-      ) : (
-        "Checkmate! Please submit your moves on chain to complete the game."
-      )}
-    </ModalBody>
-    <ModalFooter>
-      {wagerStatus === null && (
-        <Flex width="100%" justifyContent="center" alignItems="center">
-          <Box
-            width="40px"
-            height="40px"
-            marginRight="2"
-            visibility="hidden"
-          />
-          <Button
-            colorScheme="green"
-            onClick={async () => {
-              setIsSubmitting(true);
-              try {
-                await onSubmitMoves(gameWager);
-                setWereMovesSubmitted(true);
-                onClose();
-                const newGameSound = new Audio('/sounds/GenericNotify.mp3');
-                newGameSound.play();
-              } catch (error) {
-                console.error('Error during submit:', error);
-              } finally {
-                setIsSubmitting(false);
-              }
-            }}
-          >
-            Submit moves on chain
-          </Button>
-          {isSubmitting ? (
-            <Spinner
-              color="green.500"
-              width="40px"
-              height="40px"
-              marginLeft="2"
-            />
+              <VStack align="start" spacing={2}>
+                <HStack>
+                  <Icon as={FaUserCircle} color="blue.500" />
+                  <Text fontSize="md" fontWeight="semibold">
+                    Player {wagerPlayers?.player0}:{' '}
+                    <chakra.span color="green.500">
+                      {wagerStatus.winsPlayer0} Wins
+                    </chakra.span>
+                  </Text>
+                </HStack>
+                <HStack>
+                  <Icon as={FaUserCircle} color="red.500" />
+                  <Text fontSize="md" fontWeight="semibold">
+                    Player {wagerPlayers?.player1}:{' '}
+                    <chakra.span color="green.500">
+                      {wagerStatus.winsPlayer1} Wins
+                    </chakra.span>
+                  </Text>
+                </HStack>
+              </VStack>
+            </>
           ) : (
-            <Box
-              width="40px"
-              height="40px"
-              marginLeft="2"
-              visibility="hidden"
-            />
+            'Checkmate! Please submit your moves on chain to complete the game.'
           )}
-        </Flex>
-      )}
-    </ModalFooter>
-  </ModalContent>
-</Modal>
-
+        </ModalBody>
+        <ModalFooter>
+          {wagerStatus === null && (
+            <Flex width="100%" justifyContent="center" alignItems="center">
+              <Box
+                width="40px"
+                height="40px"
+                marginRight="2"
+                visibility="hidden"
+              />
+              <Button
+                colorScheme="green"
+                onClick={async () => {
+                  setIsSubmitting(true);
+                  try {
+                    await onSubmitMoves(gameWager);
+                    setWereMovesSubmitted(true);
+                    onClose();
+                    const newGameSound = new Audio('/sounds/GenericNotify.mp3');
+                    newGameSound.play();
+                  } catch (error) {
+                    console.error('Error during submit:', error);
+                  } finally {
+                    setIsSubmitting(false);
+                  }
+                }}
+              >
+                Submit moves on chain
+              </Button>
+              {isSubmitting ? (
+                <Spinner
+                  color="green.500"
+                  width="40px"
+                  height="40px"
+                  marginLeft="2"
+                />
+              ) : (
+                <Box
+                  width="40px"
+                  height="40px"
+                  marginLeft="2"
+                  visibility="hidden"
+                />
+              )}
+            </Flex>
+          )}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 };
 
